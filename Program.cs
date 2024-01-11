81ebc265ab810a25853aa3d7ca66b6531651ff14
michaumar <michal.maruszczak@o2.pl>
2024-01-11 17:39:31 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using TrainingApp2;
+
+var date1 = new DateTime(33, 1, 1, 0, 0, 0); //Y, M, D, H, Min, S 
+Console.WriteLine(date1);
+// For en-US culture, displays 3/1/2008 7:00:00 AM
+
+
+
+
+Console.WriteLine(DateTime.Now.Day);
+
+
+
+
+//Project project = new Project(name, description, startTime, endTime);
---
+++ Project.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingApp2
+{
+    public class Project
+    {
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private string description;
+
+		public string Description
+		{
+			get { return description; }
+			set { description = value; }
+		}
+
+		private DateTime startTime;
+
+		public DateTime StartTime
+        {
+			get { return startTime; }
+			set { startTime = value; }
+		}
+
+		private DateTime endTime;
+
+		public DateTime EndTime
+        {
+			get { return endTime; }
+			set { endTime = value; }
+		}
+
+		public Project(string name, string description, DateTime startTime, DateTime endTime)
+		{
+			Name = name;
+			Description = description;
+			StartTime = startTime;
+			EndTime = endTime;
+		}
+
+
+	}
+}
